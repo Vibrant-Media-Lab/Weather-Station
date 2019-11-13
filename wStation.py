2d55297f75dd3c9f9c47f810a7d2900330034a2e
coachuhar <cml120@pitt.edu>
2019-11-12 21:07:22 -0500

    Code cleaning/ Updated CSS

--- wStation.py
+++ wStation.py
@@ -17,8 +17,7 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
+
 sheet = client.open("VML WeatherStation Historical Data").sheet1
 
 
@@ -36,38 +35,6 @@ exampleData = {
 
 }
 
-exampleHistData = [{
-                "Temperature": 1,
-                "pressure": "thing",
-                "humidity": "thing",
-                "Air Quality ": "thing",
-                "Wind Speed": "thing",
-                "Wind Heading": "thing",
-                "Rain Rate": "thing"
-            },
-            {
-                "Temperature": 1,
-                "pressure": "thing",
-                "humidity": "thing",
-                "Air Quality ": "thing",
-                "Wind Speed": "thing",
-                "Wind Heading": "thing",
-                "Rain Rate": "thing"
-            },
-            {
-                "Temperature": 1,
-                "pressure": "thing",
-                "humidity": "thing",
-                "Air Quality ": "thing",
-                "Wind Speed": "thing",
-                "Wind Heading": "thing",
-                "Rain Rate": "thing"
-            }
-]
-
-
-
-
 #where to put shutdown
 
 @app.route('/')
@@ -79,13 +46,11 @@ def hello_world():
 
 #curl -XPOST -H "Content-Type: application/json" 'http://127.0.0.1:5000/?thing1=1' -d '{"thing2":2}'
 
-#Two things activate this, doing a refresh 
-#Or having a count down until the next get - or at least itme stamp it 
-#Eventually have to make this work with multiple nodes 
+#TODO work with multiple weather nodes
+#TODO make timing more robust so there is no possibility of desyncronization and rewriting of same data 
 @app.route("/get_data", methods=['GET', 'POST'])
 def get_data():
     #need to handle empty data here 
-
     #Get updated data and put it into
     #append current time again  
     
@@ -95,21 +60,22 @@ def get_data():
     #immediately call function to store it in the db 
    # return json.dumps(params)
    exampleD = json.dumps(exampleData)
+   writeData(exampleD)
    return exampleD
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func = get_data, trigger="interval", minutes = 60)
+
 
 
 
 
 #Write Data to Google Sheet
-def writeData():
+#TODO Change this to write only when it gets new data 
+def writeData(jsonData):
     row = []
     #current time append
     row.append(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
 
-    data = json.loads(get_data())
+    data = json.loads(jsonData)
 
     
     for item in data.items():
@@ -121,24 +87,24 @@ def writeData():
 
     return 1
 
-scheduler.add_job(func = writeData, trigger="interval", minutes = 60)
+
+#Schedule Jobs to run every hour
+scheduler = BackgroundScheduler()
+#For testing purposes
+#scheduler.add_job(func = get_data, trigger="interval", seconds = 3)
+
+scheduler.add_job(func = get_data, trigger="interval", minutes = 60)
+
+
+#originally had writeData as a job
 scheduler.start()
 
+#TODO Deprecated for now, original plan was to pull from a database. 
 @app.route("/get_histo", methods=['GET', 'POST'])
 def get_histo():
     return exampleHistData
 
-# handle hsitory and store it 
-
-#Get input event, how to monitor post requests
-#Justify why baseline is better 
-
-#What it has logged
-    #summed up from the last 24 hours, long term logging
-    #csv on local sd card
-    #pushed to google doc to put everything together
-    #link to this on page to see - have some kind of database 
-    #sql lite 
+#Context Processor
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
@@ -153,4 +119,4 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 
 
-#db controller and write to database with time stamp
+
